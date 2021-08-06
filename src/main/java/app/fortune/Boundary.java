ba659a456f1a9692a86a4a236a127ed0dea68fc1
chzesa <chzesa@gmail.com>
2021-08-06 18:57:30 +0300

    Add boundary endpoint calculation to boundary class

--- src/main/java/app/fortune/Boundary.java
+++ src/main/java/app/fortune/Boundary.java
@@ -15,6 +15,32 @@ public class Boundary
 		this.siteB = siteB;
 	}
 
+	public Vector begin()
+	{
+		return ray.origin;
+	}
+
+	public Vector end(double sweepline)
+	{
+		Vector[] intersections = Utils.parabolaIntersection(siteA, siteB, sweepline);
+
+		if (intersections.length == 0)
+			return ray.origin;
+
+		if (intersections.length == 1)
+			return new Vector(intersections[0].x, Utils.parabolaY(siteA, sweepline, intersections[0].x));
+
+		Vector end;
+
+		// Determine which intersection the boundary ray intersects
+		Vector delta = intersections[0].sub(ray.origin).normalize();
+
+		if (delta.equals(ray.direction.normalize()))
+			return new Vector(intersections[0].x, Utils.parabolaY(siteA, sweepline, intersections[0].x));
+		else
+			return new Vector(intersections[1].x, Utils.parabolaY(siteA, sweepline, intersections[1].x));
+	}
+
 	@Override
 	public String toString()
 	{
