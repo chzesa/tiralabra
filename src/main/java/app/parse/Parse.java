86d37fb7521b98be993d6d0c794ec64fff895ea4
chzesa <chzesa@gmail.com>
2021-08-13 05:45:45 +0300

    Remove comma from parser output

--- src/main/java/app/parse/Parse.java
+++ src/main/java/app/parse/Parse.java
@@ -14,7 +14,7 @@ public class Parse
 		StringBuilder sb = new StringBuilder();
 
 		for (Vector v : sites)
-			sb.append(v.x + ", " + v.y + "\n");
+			sb.append(v.x + " " + v.y + "\n");
 
 		sb.append("\n");
 
