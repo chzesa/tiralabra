ef4fe2d41124481020907bc543d94328108a44d8
chzesa <chzesa@gmail.com>
2021-08-13 03:13:44 +0300

    Add site list conversion to/from strings

---
+++ src/main/java/app/parse/Parse.java
@@ -0,0 +1,60 @@
+package app.parse;
+
+import app.vector.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+public class Parse
+{
+	public static String toStringl(List<Vector> sites)
+	{
+		StringBuilder sb = new StringBuilder();
+
+		for (Vector v : sites)
+			sb.append(v.x + ", " + v.y + "\n");
+
+		sb.append("\n");
+
+		return sb.toString();
+	}
+
+	public static String toStringll(List<List<Vector>> sites)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (List<Vector> l : sites)
+			sb.append(toStringl(l));
+
+		return sb.toString();
+	}
+
+	public static List<Vector> fromStringl(String s)
+	{
+		Scanner scan = new Scanner(s);
+		List<Vector> sites = new ArrayList<>();
+		try {
+			while(true)
+			{
+				double x = scan.nextDouble();
+				double y = scan.nextDouble();
+				sites.add(new Vector(x, y));
+			}
+		}
+		catch(Exception e) { }
+
+		return sites;
+	}
+
+	public static List<List<Vector>> fromStringll(String in)
+	{
+		String[] parts = in.split("\n\n");
+		ArrayList<List<Vector>> res = new ArrayList<>();
+
+		for (String s : parts)
+			res.add(fromStringl(s));
+
+		return res;
+	}
+}
