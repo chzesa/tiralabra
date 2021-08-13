a29836e37c0d5a455486288a8c99e9e145d4c7a0
chzesa <chzesa@gmail.com>
2021-08-13 14:08:49 +0300

    Add some tree tests

--- src/test/java/tree/TreeTest.java
+++ src/test/java/tree/TreeTest.java
@@ -1,9 +1,175 @@
 package app.tree;
 
+import java.util.Comparator;
 import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.*;
 
 public class TreeTest
 {
-	
+	class Cmp implements Comparator<Integer>
+	{
+		public int compare(Integer a, Integer b)
+		{
+			return Integer.compare(a, b);
+		}
+	}
+
+	Tree tree;
+
+	void balanced()
+	{
+		tree = new Tree(new Cmp());
+		tree.add(3);
+		tree.add(2);
+		tree.add(4);
+		tree.add(0);
+		tree.add(1);
+		tree.add(5);
+		tree.add(6);
+	}
+
+	void leftHeavy()
+	{
+		tree = new Tree(new Cmp());
+		for (int i = 0; i < 10; i++)
+			tree.add(9 - i);
+	}
+
+	void rightHeavy()
+	{
+		tree = new Tree(new Cmp());
+		for (int i = 0; i < 10; i++)
+			tree.add(i);
+	}
+
+	void exists(int i)
+	{
+		Tree<Integer>.Node res = tree.find(i);
+		assertNotNull(res);
+		assertTrue(res.value().equals(i));
+	}
+
+	@Test
+	public void testAdd()
+	{
+		leftHeavy();
+		rightHeavy();
+		balanced();
+	}
+
+	@Test
+	public void testFind1()
+	{
+		rightHeavy();
+
+		for (int i = 0; i < 10; i++)
+			exists(i);
+	}
+
+	@Test
+	public void testFind2()
+	{
+		leftHeavy();
+
+		for (int i = 0; i < 10; i++)
+			exists(i);
+	}
+
+	@Test
+	public void testFind3()
+	{
+		balanced();
+
+		for (int i = 0; i < 7; i++)
+			exists(i);
+	}
+
+	@Test
+	public void testPrevious1()
+	{
+		rightHeavy();
+
+		for (int i = 0; i < 10; i++)
+		{
+			Tree<Integer>.Node res = tree.previous(i + 1);
+			assertNotNull(res);
+			assertTrue(res.value().equals(i));
+		}
+
+		assertNull(tree.previous(0));
+	}
+
+	@Test
+	public void testPrevious2()
+	{
+		leftHeavy();
+
+		for (int i = 0; i < 10; i++)
+		{
+			Tree<Integer>.Node res = tree.previous(i + 1);
+			assertNotNull(res);
+			assertTrue(res.value().equals(i));
+		}
+
+		assertNull(tree.previous(0));
+	}
+
+	@Test
+	public void testPrevious3()
+	{
+		balanced();
+
+		for (int i = 0; i < 7; i++)
+		{
+			Tree<Integer>.Node res = tree.previous(i + 1);
+			assertNotNull(res);
+			assertTrue(res.value().equals(i));
+		}
+
+		assertNull(tree.previous(0));
+	}
+
+	@Test
+	public void testNext1()
+	{
+		rightHeavy();
+
+		for (int i = 0; i < 10; i++)
+		{
+			Tree<Integer>.Node res = tree.next(i - 1);
+			assertNotNull(res);
+			assertTrue(res.value().equals(i));
+		}
+
+		assertNull(tree.next(9));
+	}
+
+	@Test
+	public void testNext2()
+	{
+		leftHeavy();
+
+		for (int i = 0; i < 10; i++)
+		{
+			Tree<Integer>.Node res = tree.next(i - 1);
+			assertNotNull(res);
+			assertTrue(res.value().equals(i));
+		}
+		assertNull(tree.next(9));
+	}
+
+	@Test
+	public void testNext3()
+	{
+		balanced();
+
+		for (int i = 0; i < 7; i++)
+		{
+			Tree<Integer>.Node res = tree.next(i - 1);
+			assertNotNull(res);
+			assertTrue(res.value().equals(i));
+		}
+
+		assertNull(tree.next(6));
+	}
 }
