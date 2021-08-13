b1387643998f94c6d8fb5551b5cc963d7c90d4a4
chzesa <chzesa@gmail.com>
2021-08-13 17:55:49 +0300

    Correctly create a balanced tree in TreeTest

--- src/test/java/tree/TreeTest.java
+++ src/test/java/tree/TreeTest.java
@@ -20,11 +20,13 @@ public class TreeTest
 	{
 		tree = new Tree(new Cmp());
 		tree.add(3);
-		tree.add(2);
-		tree.add(4);
-		tree.add(0);
+
 		tree.add(1);
 		tree.add(5);
+
+		tree.add(0);
+		tree.add(2);
+		tree.add(4);
 		tree.add(6);
 	}
 
